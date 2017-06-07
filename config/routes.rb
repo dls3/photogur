5a3bbd175e318a1167d401104bb12e152e68b563
Doug <doug.steinberg@gmail.com>
2017-06-07 14:16:55 -0400

    Able to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   root 'pictures#index'
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
 end
